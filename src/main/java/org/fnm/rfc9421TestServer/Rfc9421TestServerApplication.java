a7e11cdde9abb889d127c585c10855f0b9204c3f
Martin Smock <martin_smock@me.com>
2025-11-17 09:27:07 +0100

    Initial commit

---
+++ src/main/java/org/fnm/rfc9421TestServer/Rfc9421TestServerApplication.java
@@ -0,0 +1,13 @@
+package org.fnm.rfc9421TestServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rfc9421TestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rfc9421TestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fnm/rfc9421TestServer/Rfc9421TestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fnm.rfc9421TestServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rfc9421TestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
